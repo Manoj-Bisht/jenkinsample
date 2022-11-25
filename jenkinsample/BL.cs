8ca934525066ad35026443e7e80609f7efba4d42
Manoj Kumar Bisht <manoj.kumar.bisht@gmail.com>
2022-11-25 12:59:44 +0530

    Add project files.

---
+++ jenkinsample/BL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jenkinsample
+{
+    public class BL
+    {
+        public string GetString(string s)
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ jenkinsampleTests/BLTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using jenkinsample;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jenkinsample.Tests
+{
+    [TestClass()]
+    public class BLTests
+    {
+        [TestMethod()]
+        public void GetStringTest()
+        {
+            Assert.AreEqual("Hello World", "Hello World");
+        }
+    }
+}
